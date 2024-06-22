a6476b59795be850291fbb8f229759f7c023967c
hayaalhelo <2011.hayaalhelo@gmail.com>
2024-06-22 11:14:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,25 @@ let buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
-arr.forEach(Button => {
-    buttons.addEventListner('click', (e) =>{
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
         if(e.target.innerHTML == '='){
-        string = eval(string);
-        input.value = string;
-    }
-    else if(e.target.innerHTML == 'AC'){
-        string = "";
-        input.value = string;
-    }
-    else if(e.target.innerHTML=='DEL'){
-        string=string.substring(0, string.length-1);
-        input.value = string;
-    }
-    else{
-        string += e.target.innerHTML;
-        input.value = string;
-    }
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
     })
 })
